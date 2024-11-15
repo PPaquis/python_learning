593278b56c5e0874f394c090b8582529904f38a3
ppaqui <pablo.paquis@alumnos.ucn.cl>
2024-11-15 17:26:05 +0100

    añadir script basico sobre variables python

---
+++ variables2.py
@@ -0,0 +1,11 @@
+#Variables 2
+numero_entero = 10
+numero_decimal = 3.5
+mensaje = "Aprendiendo Python y Git juntos"
+es_cierto = True
+
+print("Número entero:", numero_entero)
+print("Número decimal:", numero_decimal)
+print("Mensaje:", mensaje)
+print("¿Es cierto?:", es_cierto)
+
