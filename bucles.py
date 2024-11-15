d6f14b4bba7c2b98d225343f603f4c0e5b18e91f
ppaqui <pablo.paquis@alumnos.ucn.cl>
2024-11-15 17:47:01 +0100

    bucles y una version 1 de variables

---
+++ bucles.py
@@ -0,0 +1,9 @@
+#Bucles en python
+for i in range(5):
+	print("Esto es un bucle for, Iteracion", i)
+
+contador = 0
+while contador <3:
+	print("Esto es un bucle while", contador)
+	contador += 1
+
---
+++ variables.py
@@ -0,0 +1,18 @@
+#Este es un ejemplo basico de variables en Python
+
+#Variables de tipo entero
+numero = 10 
+print("el valor del numero es:", numero)
+
+#Variable del tipo decimal (float)
+decimal = 4.3
+print("el valor del numero es:", decimal)
+
+#Variable de tipo texto (string)
+mensaje= "Hola, python"
+print("el mensaje es:", mensaje)
+
+#Variable del tipo booleano (verdadero o falso)
+es_python_genial = True
+print("¿es python genial?:", es_python_genial)
+
